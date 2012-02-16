5277d572e150832f3b3188fa6c4645c019e30862
nescher <nescher2003@yahoo.de>
2012-02-16 22:34:44 +0100

    a

---
+++ DODtab/ThirdViewControler.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewControler.h
+//  DODtab
+//
+//  Created by Tobias Friedenauer on 14.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewControler : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *scroller;
+@end
